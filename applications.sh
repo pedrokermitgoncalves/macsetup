37ab897263bd86ade7a8463216eb509470f77f32
Pedro Gonçalves <pribeiro.goncalves@gmail.com>
2019-08-06 18:16:23 +0100

    adds office and mail apps

--- applications.sh
+++ applications.sh
@@ -6,7 +6,17 @@ sudo -v
 mas signin pribeiro.goncalves@gmail.com
 
 # install apps
-mas install 732710998 # Enpass
-mas install 425424353 # The Unarchiver
-mas install 803453959 # Slack
-mas install 497799835 # XCode
+mas install 732710998  # Enpass
+mas install 425424353  # The Unarchiver
+mas install 803453959  # Slack
+mas install 497799835  # XCode
+mas install 1147396723 # WhatsApp desktop
+mas install 1460809171 # Airmail Zero
+
+# install office apps
+mas install 985367838 # Outlook
+mas install 462054704 # Word
+mas install 462058435 # Excel
+
+# upgrade outdated apps
+mas upgrade
