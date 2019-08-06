0dd9bdfc7ee9e677d65147e3bc1b992192b2aba8
Pedro Gonçalves <pribeiro.goncalves@gmail.com>
2019-08-06 18:07:41 +0100

    adds encase to app list

--- applications.sh
+++ applications.sh
@@ -6,6 +6,7 @@ sudo -v
 mas signin pribeiro.goncalves@gmail.com
 
 # install apps
-mas install 497799835 # XCode
+mas install 732710998 # Enpass
 mas install 425424353 # The Unarchiver
 mas install 803453959 # Slack
+mas install 497799835 # XCode
