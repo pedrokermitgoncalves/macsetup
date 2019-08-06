25574a1175bd9d2ff30818cb490d015061e61910
Pedro Gonçalves <pribeiro.goncalves@gmail.com>
2019-08-06 17:44:34 +0100

    create press-up.sh

---
+++ pre-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Homebrew Homebrew #
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# TOOLS #
+brew install git
+brew install node
+brew install nvm
+brew install yarn
+
+# Fonts #
+brew tap caskroom/fonts
+brew cask install font-fira-mono
+brew cask install font-fira-mono-for-powerline
+brew cask install font-fira-sans
+brew cask install font-fira-sans-condensed
+brew cask install font-fira-code
+
+# Verify installation
+command -v git
+command -v node
+command -v nvm
+command -v npm
+command -v yarn
+
+# Remove outdated versions from the cellar
+brew cleanup
