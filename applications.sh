661b1a431d8d302dc869616f725c56899032b320
Pedro Gonçalves <pribeiro.goncalves@gmail.com>
2019-08-06 17:55:58 +0100

    adds apps to install list

---
+++ applications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+mas signin pribeiro.goncalves@gmail.com
+
+# install apps
+mas install 497799835 # XCode
+mas install 425424353 # The Unarchiver
+mas install 803453959 # Slack
