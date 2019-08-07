3eb60459f66259b1ff47581b4b4fd4f4babb27c3
Pedro "Kermit" Gonçalves <pedro.rgoncalves@prozis.com>
2019-08-07 09:22:55 +0100

    comment office apps

--- applications.sh
+++ applications.sh
@@ -6,17 +6,17 @@ sudo -v
 mas signin pribeiro.goncalves@gmail.com
 
 # install apps
-mas install 732710998  # Enpass
-mas install 425424353  # The Unarchiver
-mas install 803453959  # Slack
-mas install 497799835  # XCode
 mas install 1147396723 # WhatsApp desktop
 mas install 1460809171 # Airmail Zero
+mas install 425424353  # The Unarchiver
+mas install 497799835  # XCode
+mas install 732710998  # Enpass
+mas install 803453959  # Slack
 
 # install office apps
-mas install 985367838 # Outlook
-mas install 462054704 # Word
-mas install 462058435 # Excel
+# mas install 462054704 # Word
+# mas install 462058435 # Excel
+# mas install 985367838 # Outlook
 
 # upgrade outdated apps
 mas upgrade
@@ -25,11 +25,13 @@ mas upgrade
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install slack
+brew cask install spectacle
 brew cask install visual-studio-code
 brew cask install zeplin
-brew cask install spectacle
+brew cask install zoom
 
 # personal apps
 brew cask install google-backup-and-sync
-brew cask install vlc
 brew cask install plex-media-server
+brew cask install spotify
+brew cask install vlc
